cdd512ff368662abaa0f26e71db3d86989ee6e0b
Fhareed <72567937+Fhareed@users.noreply.github.com>
2023-11-30 10:07:13 +0100

    created login signup and navigationlinks with the auth context

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Chat from "./pages/Chat";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+function App() {
+
+
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
