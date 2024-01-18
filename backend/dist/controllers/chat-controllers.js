04b644a52db5265236fc4239df057bb1b7d22b0a
Fhareed <72567937+Fhareed@users.noreply.github.com>
2024-01-18 09:12:37 +0100

    updated the chatResponse

--- backend/dist/controllers/chat-controllers.js
+++ backend/dist/controllers/chat-controllers.js
@@ -1,3 +1,44 @@
+import User from "../models/User.js";
+import { configureOpenAI } from "../config/openai-config.js";
+import { OpenAIApi } from "openai";
 export const generateChatCompletion = async (req, res, next) => {
+    const { message } = req.body;
+    console.log(message);
+    try {
+        const user = await User.findById(res.locals.jwtData.id);
+        console.log(user);
+        if (!user)
+            return res
+                .status(401)
+                .json({ message: "User not registered OR Token malfunctioned" });
+        // grab chats of user
+        const chats = user.chats.map(({ role, content }) => ({
+            role,
+            content,
+        }));
+        //console.log("Chat response:",chats)
+        chats.push({ content: message, role: "user" });
+        user.chats.push({ content: message, role: "user" });
+        // Log for debugging
+        console.log("Chats are:", chats);
+        // send all chats with the new one to OpenAI API
+        const config = configureOpenAI();
+        const openai = new OpenAIApi(config);
+        // get the latest response
+        const chatResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: chats,
+        });
+        // Log for debugging
+        console.log("Chat response:", chatResponse);
+        user.chats.push(chatResponse.data.choices[0].message);
+        await user.save();
+        console.log("saved chats: ", user.chats);
+        return res.status(200).json({ chats: user.chats });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
 };
 //# sourceMappingURL=chat-controllers.js.map
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import { IoIosLogIn } from "react-icons/io";
 import { Box, Typography, Button } from "@mui/material";
 import CustomizedInput from "../components/shared/CustomizedInput";
+import { UserAuth } from "../context/AuthContext";
+import { toast } from "react-hot-toast";
 const Login = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const auth = UserAuth();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget)
-    const email = formData.get("email");
-    const password = formData.get("password");
-    console.log( email, password );
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    try {
+      toast.loading("Signing In", { id: "login" });
+      await auth?.login(email, password);
+      toast.success("Signed In Successfully", { id: "login" });
+    } catch (err) {
+      console.log(err);
+      toast.error("Sign In failed", { id: "login" });
+    }
   };
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
